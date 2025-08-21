2a839656270debd4ff43c143733aeeee34c056c9
thierry1p09 <thierry.flor@escola.pr.gov.br>
2025-08-20 21:04:26 -0300

    aula 10

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.sr = Scroll Reveal({ reset: true })
+window.sr = ScrollReveal({ reset: true })
 
-// sr.reveal("#image" , { duration: 1000});
+ sr.reveal("#image" , { duration: 1000});
 
 sr.reveal("#textoconteudo" , {
-  rotate: {x=0,y=100,z=0},
+  rotate: {x:0,y:100,z:0},
   duration:1500});
 
 sr.reveal("#formulario", {duration:1000});
